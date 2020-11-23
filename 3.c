f6fc224d2a4cd757fadbe327634358bd6e762045
uttappan <arjuneastkoleri@gmail.com>
2020-11-24 00:29:43 +0530

    first commit

---
+++ 3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+float a,b,s;
+printf("Enter the first  number\n");
+scanf("%f",&a);
+printf("Enter the second number\n");
+scanf("%f",&b);
+s=(a+b)/2;
+printf("The Average of %f and %f = %f\n",a,b,s);
+} 
+
+
