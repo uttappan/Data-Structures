bcb718fcf4651722c54c77b9dacc9e47e0bac2ec
uttappan <arjuneastkoleri@gmail.com>
2020-11-24 10:42:24 +0530

    first commit

---
+++ 5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,s;
+printf("Enter the first  number\n");
+scanf("%d",&a);
+printf("Enter the second number\n");
+scanf("%d",&b);
+s=a*b;
+printf("The Product of %d and %d = %d\n",a,b,s);
+} 
+
+
